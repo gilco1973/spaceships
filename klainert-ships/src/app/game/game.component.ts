e34eba2ef464fef40c0c307408faac26887390b1
Gil Klainert <gil.klainert@gmail.com>
2019-12-29 13:00:55 +0200

    arrows keys

--- klainert-ships/src/app/game/game.component.ts
+++ klainert-ships/src/app/game/game.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GameService } from '../game.service';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, AfterViewInit {
+  @HostListener('document:keydown.arrowleft')
+moveLeft() {
+  console.log('listener key left');
+}
+
+
+@HostListener('document:keydown.arrowright')
+moveRight() {
+  console.log('listener key right');
+}
   ship: any;
 
   constructor(private gameService: GameService) {
@@ -17,5 +28,27 @@ export class GameComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  ngAfterViewInit(): void {
+    var element = document.getElementById('ship1');
+    element.className= 'ship';
+    element.style.backgroundImage = 'url('+this.ship.img +')';
+  }
+  keyClicked(event){
+    var element = document.getElementById('ship1');
+    switch(event.keyCode){
+      case 37:
+        console.log('key left');
+        
+        break;
+        case 38:
+        console.log('key up');
+        break;
+        case 39:
+        console.log('key right');
+        break;
+        case 40:
+        console.log('key down');
+        break;
+    }
+  }
 }
